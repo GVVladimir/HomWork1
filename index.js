21f19a5db02c35e3830516ecefa81e6b0a2d5b51
Vladimir <vladimir.grishaew@yandex.ru>
2023-05-16 14:26:11 +0300

    hw3

--- index.js
+++ index.js
@@ -1,79 +1,115 @@
-// let a = 10;
+// задание 1
+// let password = String(prompt('Введите пароль: '));
+// password = password.toLocaleLowerCase();
 
-// alert (a);
-
-// a = 20;
-
-// alert (a);
-
-// let ferstIphone = '9 января 2007 года';
-// alert (`Создание первого iphone ${ferstIphone}`);
-
-// let docJs = 'Брендан Эйх';
-// alert (`Создатель языка JavaScript - ${docJs}!`);
-
-// let ferst = 10;
-// let second = 2;
-
-// alert (ferst + second);
-// alert (ferst - second);
-// alert (ferst * second);
-// alert (ferst / second);
-
-// let c = 2;
-// let result = c ** 5;
-// alert (result);
-
-
-// a = 9;
-// let b = 2;
-
-// alert(a % b);
-
-// let num = 1;
-// num += 5;
-// num -= 3;
-// num *= 7;
-// num /= 3;
-// num ++ ;
-// num -- ;
-// alert(num);
-
-// let age = prompt(`Сколько Вам лет`);
-// alert(age);
-
-// let user = {
-//     age: Namber(40), 
-//     name: 'Vladimir',
-//     isAdmin: true
-// }
-// alert(prompt(user));
-
-
-
-// let user = {
-//     age: 40, 
-//     name: 'Vladimir',
-//     isAdmin: true,
-//     "city of residence": true
+// if(password === 'пароль') {
+//     console.log ('Пароль введен верно');
+// } else {
+//     console.log ('Пароль введен неправильно');
 // }
 
-// let user = {
-//     gender: 'man' , 
-//     name: 'Vladimir',
-//     isAdmin: true,
-//     "city of residence": true
+
+// задание 2
+// let c = prompt('введите число');
+// (c > 0 && c <10) ? console.log ('Верно') : console.log('Неверно');
+
+// задание 3
+// let d = prompt('Введите первое число');
+// let c = prompt('Введите второе число');
+
+// (d > 100 || c > 100) ? console.log('Верно') : console.log('Неверно');
+
+// задание 4
+// let a = 2;
+// let b = 3;
+
+//     console.log(a+b);
+
+// задание 5
+// let monthNumber = prompt('Введите месяц');
+// monthNumber = monthNumber.toLocaleLowerCase();
+
+// switch (monthNumber) {
+//     case '1':
+//    console.log('Зима');        
+//     break; 
+//     case '2':
+//     console.log('Зима');        
+//     break;
+//     case '12':
+//     console.log('Зима');        
+//     break;
+//     case '3':
+//     console.log('Весна');        
+//     break; 
+//     case '4':
+//     console.log('Весна');
+//     break;
+//     case '5':
+//     console.log('Весна');
+//     break;
+//     case '6':
+//     console.log('Лето');        
+//     break; 
+//     case '7':
+//     console.log('Лето');
+//     break;
+//     case '8':
+//     console.log('Лето');
+//     break;   
+//     case '9':
+//     console.log('Осень');        
+//     break; 
+//     case '10':
+//     console.log('Весна');
+//     break;
+//     case '11':
+//     console.log('весна') ;  
+//     default:
+//     console.log('Такого месяца нет')
+//         break;
 // }
 
-// delete user["city of residence"];
+// задание 7
+// let randomNumber = Number(prompt('Пожалуйста, введите любое число'));
+// randomNumber = Number(randomNumber);
 
-// let info = prompt("Какую информацию хотите узнать о пользователе?")
+// if(randomNumber % 2 === 0) {
+//     console.log('Число четное!');
+// } else {
+//     console.log('Нечетное число!')
+// }
 
-// alert(user.info);
+// задание 8
+// let clientOS = prompt('Какая у Вас IOS?');
+// let a = 1;
+// let b = 0;
 
-let name = prompt('Ваше имя?');
-alert('Привет: ' + name + "!");
+// if(clientOS === '1'){
+//     console.log('Установите версию приложения для Android по ссылке');
+// } else if(clientOS === '0') {
+//     console.log('Установите версию приложения для iOS по ссылке');
+// } else {
+//     console.log('Что то пошло не так');
+// }
 
+// задание 9
+// let clientOS = prompt('Какая у Вас IOS?');
+// const clientDeviceYear = prompt('Кокого года Ваш телефон?');
 
+// if(clientOS === '1'){
+//     if(clientDeviceYear < 2015){
+//         console.log('Установите облегченную версию приложения для Android по ссылке');
+//     }else{
+//     console.log('Установите версию приложения для Android по ссылке');
+// }
+// } else if(clientOS === '0') {
+//     if(clientDeviceYear < 2015){
+//         console.log('Установите облегченную версию приложения для iOS по ссылке');
+//     } else {
+//         console.log('Установите версию приложения для iOS по ссылке');
+//     } 
 
-
+// } else {
+//     console.log('Что то пошло не так');
+// }
