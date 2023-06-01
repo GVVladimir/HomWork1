6cfeb9f98980f14bd283cde0e476e89b2ad49702
Vladimir <vladimir.grishaew@yandex.ru>
2023-06-01 16:41:52 +0300

    hw7

--- massiv.js
+++ massiv.js
@@ -29,16 +29,12 @@
 
 //     for ( let n = 0; n < 3; n++){
 //         arr[i][n] = 1;
-
 //     }
-   
 // } console.log(arr);
 
 // задание 5
 // let arr = [1, 1, 1];
-
 // arr.push(2, 2, 2);
-   
 //     console.log(arr);
 
 // задание 6
@@ -49,3 +45,70 @@
 // console.log(arr);
 
 
+// задание 7
+
+// let arr = [9, 8, 7, 6, 5];
+
+// let result = prompt('Введите число от 1 до 10');
+
+//  for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == result) {
+//         console.log(`есть такое число`)
+//     } else {
+//         console.log('нет такого числа')
+//     }
+//  }
+
+// задание 8
+    
+// let str = 'abcdef';
+// console.log(str);
+
+// str = str.split('');
+// console.log(str);
+
+// str = str.reverse();
+// console.log(str);
+
+// str = str.join('');
+// console.log(str);
+
+// задание 9
+// let arr = [[1, 2, 3,],[4, 5, 6]];
+
+// console.log(arr);
+
+// arr = arr.flat();
+
+// console.log(arr);
+
+// задание 10
+// let arr = [1, 3, 4, 6, 7, 9];
+// let sum = 0;
+// for (let i = 0; i < arr.length-1; i++) {
+//     sum = arr[i] + arr[i +1];
+
+//     console.log(sum);
+// };
+
+// задание 11
+// let arr = [1, 3, 5, 6, 3, 7,8];
+// console.log(arr);
+
+// let arry = arr.map(i => i**2);
+// console.log(arry);
+
+// задание 12
+// let arr = ['слово', '', 'слог', 'длинное предложение', 'буква'];
+// console.log(arr);
+
+// const arrLength = arr => arr.map(word => word.length);
+// console.log(arrLength(arr));
+
+// задание 13
+
+// let array = [-1, 0, 5, -10, 56]
+
+// const filterPositive = array => array.filter(i => i < 0);
+    
+// console.log(filterPositive(array));
